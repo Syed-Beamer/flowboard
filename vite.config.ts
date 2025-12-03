a598c5818212c0779ab7e1448bbb74494a89aae9
SYEDAHAMED29 <syed.ahamed@getbeamer.com>
2025-12-04 01:19:17 +0530

    Refactor: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,22 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8081,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    server: {
+      host: "::",
+      port: 8081,
     },
-  },
-}));
+    base: isProd ? "/flowboard/" : "/", // 👈 ONLY for production builds
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+      Boolean
+    ),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
