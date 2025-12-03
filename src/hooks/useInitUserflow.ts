6e4b077121b6116c87330e452d1d4513ff6611a0
SYEDAHAMED29 <syed.ahamed@getbeamer.com>
2025-12-04 01:54:09 +0530

    ADD: signed_up_at attribute

--- src/hooks/useInitUserflow.ts
+++ src/hooks/useInitUserflow.ts
@@ -2,12 +2,14 @@ import { useEffect } from "react";
 import userflow from "userflow.js";
 
 export function useInitUserflow() {
+  const date = new Date();
+
   useEffect(() => {
     userflow.init("ct_us1_ouxvbpelrzcefkxzgrbuklw7xe"); // Your token for Production
     userflow.identify("USER_ID", {
       name: "USER_NAME",
       email: "USER_EMAIL",
-      signed_up_at: "USER_SIGNED_UP_AT",
+      signed_up_at: date.toISOString(),
     });
   }, []);
 }
